35f7049a56e9d975aa0d7194b6fce61c248f6fb9
Cici Du <cicidhz@gmail.com>
2021-11-19 20:44:12 -0800

    Edit q9

--- demo.py
+++ demo.py
@@ -14,6 +14,11 @@ Options:
 
 from docopt import docopt
 opt = docopt(__doc__)
-print(opt)
-print(type(opt))
-print(opt['<arg4>'])
+
+def main(var):
+    print(opt)
+    print(type(opt))
+    print(opt['<arg4>'])
+
+if __name__== "__main__":
+    main(opt)
