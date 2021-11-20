c3b08b475b505949eb7b491c67f703376d37ee39
Cici Du <cicidhz@gmail.com>
2021-11-19 20:38:04 -0800

    Edit q9

---
+++ demo.py
@@ -0,0 +1,19 @@
+# author: Cici Du
+# date: 2021-11-19
+
+"""This script prints out docopt args.
+Usage: demo.py <arg1> [<arg4>] --arg2=<arg2> [--arg3=<arg3>]
+
+Options:
+<arg>             Takes any value (this is a required positional argument)
+[<arg4>]          Takes any value (this is an optional positonal argument)
+--arg2=<arg2>     Takes any value (this is a required option)
+[--arg3=<arg3>]   Takes any value (this is an optional option)
+
+""" 
+
+from docopt import docopt
+opt = docopt(__doc__)
+print(opt)
+print(type(opt))
+print(opt['<arg4>'])
